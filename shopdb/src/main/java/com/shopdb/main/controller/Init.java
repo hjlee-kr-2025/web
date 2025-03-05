8ee4811b101eee045d5efecd1017061db8a48d76
hjlee-kr-2025 <hjlee-kr@naver.com>
2025-03-05 16:14:19 +0900

    250305-Init 클래스 작성

--- shopdb/src/main/java/com/shopdb/main/controller/Init.java
+++ shopdb/src/main/java/com/shopdb/main/controller/Init.java
@@ -3,6 +3,12 @@ package com.shopdb.main.controller;
 import java.util.HashMap;
 import java.util.Map;
 
+import com.shopdb.board.dao.BoardDAO;
+import com.shopdb.board.service.BoardDeleteService;
+import com.shopdb.board.service.BoardListService;
+import com.shopdb.board.service.BoardUpdateService;
+import com.shopdb.board.service.BoardViewService;
+import com.shopdb.board.service.BoardWriteService;
 import com.shopdb.board.service.Service;
 import com.shopdb.main.dao.DAO;
 
@@ -24,7 +30,29 @@ public class Init {
 	
 	// 한번만 초기화 하는 메서드 - static 메서드 사용
 	static {
+		// 일반게시판
+		// dao를 생성
+		daoMap.put("boardDAO", new BoardDAO());
+		// 일반게시판 서비스 생성
+		// new BoardListService() ==> 주소값
+		// key : url, value : 주소값
+		serviceMap.put("/board/list.do", new BoardListService());
+		serviceMap.put("/board/view.do", new BoardViewService());
+		serviceMap.put("/board/write.do", new BoardWriteService());
+		serviceMap.put("/board/update.do", new BoardUpdateService());
+		serviceMap.put("/board/delete.do", new BoardDeleteService());
 		
+		// 조립 dao -> service 에서 사용할 수 있도록
+		serviceMap.get("/board/list.do").setDAO(daoMap.get("boardDAO"));
+		serviceMap.get("/board/view.do").setDAO(daoMap.get("boardDAO"));
+		serviceMap.get("/board/write.do").setDAO(daoMap.get("boardDAO"));
+		serviceMap.get("/board/update.do").setDAO(daoMap.get("boardDAO"));
+		serviceMap.get("/board/delete.do").setDAO(daoMap.get("boardDAO"));
+	}
+	
+	public static Service get(String uri) {
+		// uri 키값으로 서비스주소를 리턴
+		return serviceMap.get(uri);
 	}
 	
 }
