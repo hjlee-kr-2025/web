dc32d5de7a3806229a4a174adc31cc0d6e463339
hjlee <hjlee-kr@naver.com>
2025-03-31 16:40:39 +0900

    250331-7교시F

---
+++ gyshop/src/main/java/com/gyshop/member/dao/MemberDAO.java
@@ -0,0 +1,25 @@
+package com.gyshop.member.dao;
+
+import com.gyshop.main.dao.DAO;
+import com.gyshop.member.vo.MemberVO;
+
+public class MemberDAO extends DAO {
+
+	// 3. 회원가입
+	public Integer write(MemberVO vo) throws Exception {
+		// 결과 저장 변수 선언 및 초기화
+		Integer result = null;
+		
+		try {
+			
+		} catch (Exception e) {
+			// TODO: handle exception
+			e.printStackTrace();
+		} finally {
+			
+		}
+		
+		// 결과 리턴
+		return result;
+	}
+}
